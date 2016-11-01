1d49b8d5f7adedd1d8a8ae8af5ef37a615b57e70
martocci <krista.martocci@namely.com>
2016-11-01 17:55:13 -0400

    adding initial file

---
+++ bomberman/bomberman.py
@@ -0,0 +1,35 @@
+# https://www.hackerrank.com/challenges/bomber-man
+
+def write_output():
+    '''
+    
+    '''
+
+def check_cycle():
+    '''
+    The solution seems to stabilize to a cycle betwee 2 solutions that happen on t & t-4.
+    Compare the states of the system at these times and stop the code if this happens
+    '''
+    pass
+
+def read_input():
+    '''
+    Input:
+    R C N
+    grid of initial bombs
+
+    R = # of rows
+    C = # of columns
+    N = # of seconds
+
+    Input example:
+    6 7 3
+    .......
+    ...O...
+    ....O..
+    .......
+    OO.....
+    OO.....
+
+    '''
+    pass
