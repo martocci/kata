b94b008870bc066d019ca7ec19a82f11e965b2e4
martocci <krista.martocci@namely.com>
2016-11-02 09:37:50 -0400

    Exits correctly and fixed the exit solution

--- bomberman/bomberman.py
+++ bomberman/bomberman.py
@@ -5,59 +5,49 @@ def write_output(dict_final, row_total, column_total):
     Output . where there is an entry in the key
     '''
 
-    #print("dict_final = ", dict_final)
-
     final_state = [["0"]*(column_total) for n in range(row_total)]
 
     for key in dict_final:
         #print("key = ", key)
         final_state[key[0]][key[1]] = "."
-        #print(final_state)
-
-    #print('final state = ', final_state)
 
     for row in final_state:
         row_string = ''.join(row)
         print(row_string)
+
     exit()
 
-def time_pass():
+def insert_position(state_dict, row_count, cell_count, row_total, column_total):
     '''
-    Advance time step by 2 seconds
+    Insert a bomb position + its neighbors
     '''
-    pass
+
+    state_dict[(row_count, cell_count)] = None
+    if row_count > 0:
+        state_dict[(row_count - 1, cell_count)] = None
+    if row_count < row_total - 1:
+        state_dict[(row_count + 1, cell_count)] = None
+    if cell_count > 0:
+        state_dict[(row_count, cell_count - 1)] = None
+    if cell_count < column_total - 1:
+        state_dict[(row_count, cell_count + 1)] = None
+
+    return state_dict
 
 
 
-# Input:
-# R C N
-# grid of initial bombs
+# row_0 = "6 7 11"
 #
-# R = # of rows
-# C = # of columns
-# N = # of seconds
-#
-# Input example:
-# 6 7 3
-# .......
-# ...O...
-# ....O..
-# .......
-# OO.....
-# OO.....
+# row_initial = [
+#             ".......",
+#             "...0...",
+#             "....0..",
+#             ".......",
+#             "00.....",
+#             "00....."]
 
-row_0 = "6 7 3"
-
-row_initial = [
-            ".......",
-            "...0...",
-            "....0..",
-            ".......",
-            "00.....",
-            "00....."]
-
-# row_0 = "3 3 9"
-# row_initial = ["0.0", ".0.", "..."]
+row_0 = "3 3 13"
+row_initial = ["0.0", ".0.", "..."]
 
 row_0 = row_0.split(" ")
 
@@ -87,54 +77,38 @@ for row in row_initial:
     for cell in row:
         if cell == "0":
             # This is a bomb's position
-            state_dict[(row_count, cell_count)] = None
-            # Add all of the neighboring cells:
-            if row_count > 0:
-                state_dict[(row_count-1, cell_count)] = None
-            if row_count < row_total-1:
-                state_dict[(row_count+1, cell_count)] = None
-            if cell_count > 0:
-                state_dict[(row_count, cell_count - 1)] = None
-            if cell_count < column_total-1:
-                state_dict[(row_count, cell_count + 1)] = None
+            state_dict = insert_position(state_dict, row_count, cell_count, row_total, column_total)
         cell_count = cell_count + 1
     row_count = row_count + 1
 
-state_dict_1 = state_dict.copy()
-state_dict_2 = {}
-state_dict = {}
+#Keep a record of past timestep states, so the code will stop when a stable cycle is reached
+state_dict_1 = {}
 
-for time in range(time_final+1):
-    #print("time = ", int(time*2 + 1), time_final)
-    if int(time*2 + 1) == time_final+2:
-        # reached the final time
-        write_output(state_dict_1, row_total, column_total)
+for time in range(int((time_final-1)/2)):
+
+    print("time = ", (time*2 + 3), time, time_final)
+
+    state_dict_2 = state_dict_1.copy()
+    state_dict_1 = state_dict.copy()
+    state_dict = {}
 
     for row_count in range(row_total):
         for cell_count in range(column_total):
-            if (row_count,cell_count) not in state_dict_1:
-                state_dict[(row_count,cell_count)] = None
-                if row_count > 0:
-                    state_dict[(row_count - 1, cell_count)] = None
-                if row_count < row_total-1 :
-                    state_dict[(row_count + 1, cell_count)] = None
-                if cell_count > 0:
-                    state_dict[(row_count, cell_count - 1)] = None
-                if cell_count  < column_total-1:
-                    state_dict[(row_count, cell_count + 1)] = None
+            if (row_count, cell_count) not in state_dict_1:
+                state_dict = insert_position(state_dict, row_count, cell_count, row_total, column_total)
             cell_count = cell_count + 1
         row_count = row_count + 1
-    #print("time = ", int(time*2 + 1))
-    #print("state_dict = ", state_dict)
-    #print("new state = ", state_dict)
-    if state_dict == state_dict_2 and int(time%2) == 0:
+
+    print("state = ", state_dict)
+    print("state1 = ", state_dict_1)
+    print("state2 = ", state_dict_2)
+
+    if state_dict == state_dict_2 and int((time_final-2*time+3)%4) == 0:
         # reached a "stable" solution
+        print("exit with stable soln at t = ", (time*2 + 5), time)
         write_output(state_dict, row_total, column_total)
-    else:
-        # move on to the next time step
-        state_dict_2 = state_dict_1.copy()
-        state_dict_1 = state_dict.copy()
-        state_dict = {}
+
+write_output(state_dict_1, row_total, column_total)
 
 
 
